13dd5d395b9d9463356a6d2fcbb8670b606437e1
Sarah Vetrano <vetransc@dukes.jmu.edu>
2020-09-24 22:17:14 -0400

    blog 3

---
+++ sept25.js
@@ -0,0 +1,17 @@
+function open(evt, className){
+    var i;
+    var tabcontent;
+    var tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i=0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace(" active ", "");
+    }
+
+    document.getElementById(className).style.display = "block";
+    evt.currentTarget.className += " active";
+}
